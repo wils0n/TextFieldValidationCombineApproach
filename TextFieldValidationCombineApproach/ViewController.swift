edd3cf34a75d4b821e3c0823fe6acffae0b39a68
Wilson <wilson@mejia.io>
2020-07-18 07:55:17 -0500

    Initial Commit

---
+++ TextFieldValidationCombineApproach/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldValidationCombineApproach
+//
+//  Created by Wilson on 7/18/20.
+//  Copyright © 2020 NinjaLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
